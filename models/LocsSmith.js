355ff5c9ec3a66dedd37e9b34690aaf8ea94f743
SymPlee Rich <richard.dorleans@yahoo.com>
2018-10-18 01:38:31 -0500

    brought over files from 1st attempt

---
+++ models/LocsSmith.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const locsSmithSchema = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    lastname: { type: String, required: true },
+    firstname: { type: String, required: true },
+    addr: { type: String, required: true },
+    phone: { type: Number, required: false },
+    email: { type: String, required: false },
+    stylistorclient: { type: Boolean, required: true },
+    clientrating: { type: Number, required: false },
+    braiding: { type: Boolean, required: true },
+    hairlocs: { type: Boolean, required: true }
+});
+
+const LocsSmith = mongoose.model("LocsSmith", locsSmithSchema);
+
+module.exports = LocsSmith;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8080;
 const path = require('path');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(`${__dirname}/../react-client/dist`));
@@ -12,6 +13,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../react-client/dist/index.html`));
 });
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}!`);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
